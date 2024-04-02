e682536e7162a4271c8195af0be04aa7e4025d5c
Miguel Chito <96595540+donxito@users.noreply.github.com>
2024-04-02 18:26:00 +0200

    set up emailJs for contact form

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,8 +1,36 @@
 import styles from "./contact.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedinIn, faGithub } from "@fortawesome/free-brands-svg-icons";
+import emailjs from "@emailjs/browser";
+import { useRef, useState } from "react";
 
 const Contact = () => {
+  const [submitting, setSubmitting] = useState(false);
+
+  const form = useRef();
+
+  const sendEmail = (event) => {
+    event.preventDefault();
+    setSubmitting(true);
+
+    emailjs
+      .sendForm("service_ckihhci", "template_jnb6ota", form.current, {
+        publicKey: "jNLWL1qxgeLsGTWa6",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+          event.target.reset();
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      )
+      .finally(() => {
+        setSubmitting(false);
+      });
+  };
+
   return (
     <section className={styles.container} id="contact">
       <h2 className={styles.title}>Contact</h2>
@@ -10,9 +38,19 @@ const Contact = () => {
       <p className={styles.description}>
         Please fill out the form bellow to discuss any work opportunities
       </p>
-      <form className={styles.form}>
-        <input className={styles.name} type="text" placeholder="Your name" />
-        <input className={styles.email} type="email" placeholder="Your email" />
+      <form ref={form} onSubmit={sendEmail} className={styles.form}>
+        <input
+          className={styles.name}
+          type="text"
+          placeholder="Your name"
+          name="user_name"
+        />
+        <input
+          className={styles.email}
+          type="email"
+          placeholder="Your email"
+          name="user_email"
+        />
         <textarea
           className={styles.message}
           name="message"
@@ -20,10 +58,17 @@ const Contact = () => {
           placeholder="Your message"
         ></textarea>
 
-        <button className={styles.submit} type="submit" value="Send">
-          Submit
-        </button>
+        {submitting ? (
+          <button className={styles.submit} type="button" disabled>
+            Submitting...
+          </button>
+        ) : (
+          <button className={styles.submit} type="submit" value="Send">
+            Submit
+          </button>
+        )}
       </form>
+
       <div className={styles.socialIcons}>
         <a
           href="https://www.linkedin.com/in/miguelchito-reactdeveloper/"
