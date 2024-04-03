d0f957cc10d1d700acb4da5b3b7cc257c8d6bfa3
Miguel Chito <96595540+donxito@users.noreply.github.com>
2024-04-03 17:43:12 +0200

    update contact form with emailjs

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,8 +1,12 @@
+/* eslint-disable react/no-unescaped-entities */
 import styles from "./contact.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedinIn, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope, faFile } from '@fortawesome/free-solid-svg-icons'
 import emailjs from "@emailjs/browser";
 import { useRef, useState } from "react";
+import { GlowCapture, Glow } from "@codaworks/react-glow";
+import cv from "../../assets/MiguelChitoResume.pdf"
 
 const Contact = () => {
   const [submitting, setSubmitting] = useState(false);
@@ -14,15 +18,16 @@ const Contact = () => {
     setSubmitting(true);
 
     emailjs
-    .sendForm(
-      `${import.meta.env.VITE_SERVICE_ID}`,
-      `${import.meta.env.VITE_TEMPLATE_ID}`,
-      form.current,
-      `${import.meta.env.VITE_PUBLIC_KEY}`
-    )
+      .sendForm(
+        `${import.meta.env.VITE_SERVICE_ID}`,
+        `${import.meta.env.VITE_TEMPLATE_ID}`,
+        form.current,
+        `${import.meta.env.VITE_PUBLIC_KEY}`
+      )
       .then(
-        () => {
+        (result) => {
           console.log("SUCCESS!");
+          console.log(result);
           event.target.reset();
         },
         (error) => {
@@ -38,20 +43,30 @@ const Contact = () => {
     <section className={styles.container} id="contact">
       <h2 className={styles.title}>Contact</h2>
 
-      <p className={styles.description}>
-        Please fill out the form bellow to discuss any work opportunities
-      </p>
+      <GlowCapture>
+        <Glow color="hsla(124, 100%, 50%, 1)">
+          <p className={styles.description}>
+            Whether you're interested in discussing a project, sharing ideas, or
+            simply dropping a friendly hello, I'm eager to hear from you! You
+            can reach out through the contact form provided below, connect with
+            me on LinkedIn, or send me an email if that suits you better.
+            Whichever method you prefer, I'm thrilled to connect and delve into
+            the potential opportunities together. Can't wait to hear from you
+            soon!
+          </p>
+        </Glow>
+      </GlowCapture>
       <form ref={form} onSubmit={sendEmail} className={styles.form}>
         <input
           className={styles.name}
           type="text"
-          placeholder="Your name"
+          placeholder="Your Name"
           name="user_name"
         />
         <input
           className={styles.email}
           type="email"
-          placeholder="Your email"
+          placeholder="Your Email"
           name="user_email"
         />
         <textarea
@@ -87,6 +102,23 @@ const Contact = () => {
         >
           <FontAwesomeIcon icon={faGithub} />
         </a>
+
+        <a
+          href="mailto:mchito@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faEnvelope} />
+        </a>
+
+        <a
+          href={cv}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faFile} />
+        </a>
+
       </div>
     </section>
   );
--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,31 +1,41 @@
 import styles from "./projects.module.css";
-import projects from "../../data/projects.json"
+import projects from "../../data/projects.json";
 import ProjecCard from "./ProjecCard";
+import { GlowCapture, Glow } from "@codaworks/react-glow";
 
 const Projects = () => {
   return (
     <section className={styles.container} id="projects">
       <h2 className={styles.title}>Projects</h2>
+      <GlowCapture>
+        <Glow color="hsla(124, 100%, 50%, 1)">
+          <p className={styles.description}>
+            Here are some of my personal projects that I have created. These
+            creations are born from my journey through learning, particularly
+            during the dynamic experience of the Ironhack remote bootcamp. Dive
+            into my imaginative solutions as I transform obstacles and glitches
+            into valuable learning experiences, crafting innovative features
+            that enhance the coding adventure!
+          </p>
+        </Glow>
+      </GlowCapture>
       <div className={styles.projects}>
-        {
-          projects.map((project, id) => {
-            return (
-              <ProjecCard
-                key={id}
-                title={project.title}
-                description={project.description}
-                image={project.image}
-                demo={project.demo}
-                repo={project.repo}
-                skills={project.skills}
-              />
-
-            )
-          })
-        }
+        {projects.map((project, id) => {
+          return (
+            <ProjecCard
+              key={id}
+              title={project.title}
+              description={project.description}
+              image={project.image}
+              demo={project.demo}
+              repo={project.repo}
+              skills={project.skills}
+            />
+          );
+        })}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
