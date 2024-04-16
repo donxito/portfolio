c7b832a5790402f92862450ffb939da02b782923
Miguel Chito <96595540+donxito@users.noreply.github.com>
2024-04-16 15:42:17 +0200

    add footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,6 +1,8 @@
 import styles from "./footer.module.css"
 const Footer = () => {
+  
   const date = new date().getFullYear()
+
   return (
     <footer className={styles.footer} id="footer">
       <p>Copyright © {date} Miguel Chito. All rights reserved.</p>
