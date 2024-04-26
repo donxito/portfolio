2cb53fe3673e92768597b0be0310ea8005991dce
Miguel Chito <96595540+donxito@users.noreply.github.com>
2024-04-26 12:35:29 +0200

    update hero component

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -2,12 +2,29 @@
 import styles from "./hero.module.css";
 import heroImage from "../../assets/hero2.png"
 
+
+// date & greeting
+
+let greeting = "";
+const date = new Date();
+let currentHour = date.getHours();
+
+if (currentHour < 12) {
+  greeting = "Good morning";
+} else if (currentHour < 18) {
+  greeting = "Good afternoon";
+} else {
+  greeting = "Good evening";
+}
+
+
+
 const Hero = () => {
   return (
     <section className={styles.container} id="home">
       <div className={styles.content}>
         <h1 className={styles.title}>
-          Hello, I'm <span>Miguel</span>
+          {greeting},<br/> I'm <span>Miguel</span>
         </h1>
         <h3 className={styles.description}>I'm a Full Stack Developer</h3>
         <a href="mailto:mchito@gmail.com" className={styles.contactBtn}>
